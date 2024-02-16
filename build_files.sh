386fd87eec3e8e1723820fbf3e4fcf870c0b3845
Fardeen Ehsan <fardeen.es7@gmail.com>
2024-02-17 00:53:14 +0600

    build file updated

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,4 @@
 #build_files.sh
 
 pip install -r requirements.txt
-python manage.py makemigrations
-python manage.py migrate
-python manage.py collectstatic --noinput
+./manage.py collectstatic --noinput
